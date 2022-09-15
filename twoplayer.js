3313366b040d4a6bf6390fe9caa3dd22ce1cac0d
JRobbo85 <jrobertson85@hotmail.co.uk>
2022-09-15 14:39:49 +0100

    update

--- twoplayer.js
+++ twoplayer.js
@@ -1,5 +1,8 @@
 const p1roll = document.getElementById("rollButton")
 const p2roll = document.getElementById("rollButton2")
+const p1hold = document.getElementById("hold1")
+const p2hold = document.getElementById("hold2")
+
 let player1score = 0
 let player2score = 0
 let randomNumber1 = 0
@@ -21,6 +24,22 @@ const diceHide = () => {
 
 const diceHide2 = () => {
     document.getElementById("diceImage2").style.visibility="hidden";
+}
+
+const holdHide1 = () => {
+    document.getElementById("hold1").style.visibility="hidden";
+}
+
+const holdHide2 = () => {
+    document.getElementById("hold2").style.visibility="hidden";
+}
+
+const holdShow1 = () => {
+    document.getElementById("hold1").style.visibility="visible";
+}
+
+const holdShow2 = () => {
+    document.getElementById("hold2").style.visibility="visible";
 }
 
 const diceCycle = () => {
@@ -73,7 +92,8 @@ const rollTheDice = () => {
         if (randomNumber1 == 1){  
             document.getElementById("diceImage").setAttribute("src", "images/dice1.png"); 
             setTimeout(function () {
-                window.confirm("You Rolled 1. You Lose...");
+                window.confirm("You Rolled 1. Change Player");
+                p2Go()
                 player1score = 0;
                 document.getElementById("score").innerHTML = player1score
                 diceHide()
@@ -100,7 +120,8 @@ const rollTheDice2 = () => {
         if (randomNumber1 == 1){  
             document.getElementById("diceImage2").setAttribute("src", "images/dice1.png"); 
             setTimeout(function () {
-                window.confirm("You Rolled 1. You Lose...");
+                window.confirm("You Rolled 1. Change Player");
+                p1Go()
                 player2score = 0;
                 document.getElementById("score2").innerHTML = player2score
                 diceHide()
@@ -122,18 +143,50 @@ const rollTheDice2 = () => {
 }
 
 
+const p1Go = () => {
+    document.getElementById("hold2").style.visibility="hidden";
+    document.getElementById("rollButton2").style.visibility="hidden";
+    document.getElementById("hold1").style.visibility="visible";
+    document.getElementById("rollButton").style.visibility="visible";
+    document.getElementById("diceImage2").style.visibility="hidden";
+}
+
+const p2Go = () => {
+    document.getElementById("hold1").style.visibility="hidden";
+    document.getElementById("rollButton").style.visibility="hidden";
+    document.getElementById("hold2").style.visibility="visible";
+    document.getElementById("rollButton2").style.visibility="visible";
+    document.getElementById("diceImage").style.visibility="hidden";
+}
 
 
 
 
-
-
-p1roll.addEventListener("click", () => {
+p1roll.addEventListener("click", () => {   
     preRoll()
     diceShow()
+    p1Go()
+})
+
+p1hold.addEventListener("click", () => {
+    document.getElementById("heldScore1").innerHTML = player1score
+    
+    
+    setTimeout(p2Go(), 10)
+    player1score = 0;
+    document.getElementById("score").innerHTML = player1score
 })
 
 p2roll.addEventListener("click", () => {
     preRoll2()
     diceShow2()
+})
+
+p2hold.addEventListener("click", () => {
+    document.getElementById("heldScore2").innerHTML = player2score
+    
+    
+    setTimeout(p1Go(), 10)
+    player2score = 0;
+    document.getElementById("score2").innerHTML = player2score
 })
